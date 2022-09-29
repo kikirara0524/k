2fe90248862536e2130b522bbb81d767d19452f8
kikirara0524 <padyopa@icloud.com>
2022-09-29 13:50:59 +0900

    kouka

--- kouka2_hiyama/settings_common.py
+++ kouka2_hiyama/settings_common.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'private_diary.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'private_diary',
+        'NAME': 'kouka2',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST':'',
